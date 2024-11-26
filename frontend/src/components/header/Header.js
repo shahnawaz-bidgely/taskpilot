992c4881716dece2f83d52f4289924c79dc60aff
shahnawaz-bidgely <102014896+shahnawaz-bidgely@users.noreply.github.com>
2024-11-26 23:48:55 +0530

    Integrated User Details Code (FE + BE)

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>TaskPilot</h1>
+    </div>
+  );
+}
+
+export default Header;
